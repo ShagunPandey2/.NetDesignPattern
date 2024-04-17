174abdd40245ae55110c1aa0f72a4dab56b5be17
ShagunPandey2 <shagun.pandey@accolitedigital.com>
2024-04-17 13:24:17 +0530

    first

---
+++ CardType.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingMall
+{
+    interface Payment
+    {
+        double discount(double amount);
+    }
+    class MembershipCard : Payment
+
+    {
+        double dis = 0.1;
+        public double discount(double amount) {
+            Console.WriteLine("You Have MemberShip Card");
+            return amount * (1 - dis);
+
+
+        }
+    }
+     class EmployeeCard : Payment
+    {
+        double dis = 0.2;
+        public double discount(double amount)
+        {
+            Console.WriteLine("Employee Card");
+            return amount * (1 - dis);
+        }
+    }
+
+    class NoCard : Payment
+    {
+        //double dis = 0.2;
+        public double discount(double amount)
+        {
+            Console.WriteLine("No Card");
+            return amount;
+        }
+    }
+
+    class DiscountFactory
+    {
+        public static Payment GetDiscount(string cardType)
+        {
+            switch (cardType)
+            {
+                case "Membership Card":
+                    return new MembershipCard();
+                case "Employee Card":
+                    return new EmployeeCard();
+                default:
+                    return new NoCard();
+            }
+
+        }
+
+        //class CardType
+        //{
+        //    static void Main(string[] args)
+        //    {
+        //        string customerCardType = "Membership Card"; 
+        //        double amount = 100.0; 
+        //        Payment discount = DiscountFactory.GetDiscount(customerCardType);
+        //        double discountedAmount = discount.discount(amount);
+        //        Console.WriteLine("Discounted amount: " + discountedAmount);
+        //    }
+        //}
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ShoppingMall
+{
+     class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Shopping Mall");
+            Console.WriteLine();
+            Console.WriteLine("Enter the Total Amount");
+            string am=Console.ReadLine();
+            int amount=Convert.ToInt32(am);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter the type of Card");
+            Console.WriteLine("Membership Card");
+            Console.WriteLine("Employee Card");
+            Console.WriteLine("No Card");
+
+            
+            string Card=Console.ReadLine();
+            Console.WriteLine();
+            //string customerCardType = "Membership Card";
+            //double amount = 100.0;
+            Payment discount = DiscountFactory.GetDiscount(Card);
+            double discountedAmount = discount.discount(amount);
+            Console.WriteLine();
+            Console.WriteLine("You Have To Pay");
+            Console.WriteLine("Discounted amount: " + discountedAmount);
+
+        }
+    }
+}
